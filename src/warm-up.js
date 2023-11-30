be15668398c51a8fa2552e0647d109dcc477a490
Tayla Phillips <richardstayla@gmail.com>
2023-11-29 20:27:18 -0600

    Complete variables warmup

--- src/warm-up.js
+++ src/warm-up.js
@@ -9,30 +9,33 @@
 // Initialize three new variables, "food", "type" and "inStock", and assign values to them with
 // a food's name, type and number of items in stock.
 
-// var food = "Ice Cream";
-// var type = "Dessert";
-// var inStock = 10
+var food = "Ice Cream";
+var type = "Dessert";
+var inStock = 10
 
 
 
 // Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
 // the inStock count double what it currently is.
-
+inStock = inStock * 2;
+console.log('double stock', inStock)
 
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
 // Use a built in math operator to decrement the inStock count by 1.
-
+inStock -= 1
+console.log('decrement stock', inStock)
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
-
+var isFavorite = true
 
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
-
+var shouldOrderMore = inStock<20
+console.log('inventory', shouldOrderMore)
 
 
 //------------------------- Conditionals -------------------------//
