849aceff686d98f0939147ff5c681c7bfc659f96
Tayla Phillips <richardstayla@gmail.com>
2023-12-01 16:22:45 -0600

    Complete 2nd order function

--- src/order.js
+++ src/order.js
@@ -4,9 +4,25 @@ function takeOrder(order, orderTypeList) {
 }}
 
 
-function refundOrder() {
+function refundOrder(orderNumber, orderArray) {
+//if orderNumber is strictly equal to orderArray[i].orderNumber, delete the object
+//from the array orderArray.
+  for (var i = 0; i < orderArray.length; i++) {
+    console.log(orderArray[i].orderNumber);
+    if (orderArray[i].orderNumber === orderNumber) {
+      console.log(`Remove order number ${orderNumber}`);
+      orderArray.splice(i, 1);
+      console.log('New Array: ', orderArray);
+    }
+  }
+  // if (orderNumber === orderArray[i].orderNumber) {
+  //     orderArray.splice(i, i)
+  //     console.log(orderArray)
+  //   } else {
+  //     return orderArray
+  //   }
+  };
 
-}
 
 
 function listItems() {
@@ -20,7 +36,7 @@ function searchOrder() {
 
 module.exports = {
   takeOrder,
-  // refundOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
