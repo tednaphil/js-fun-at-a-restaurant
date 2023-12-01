20b90e2d408cf1f792754492de2f635456d2251c
Tayla Phillips <richardstayla@gmail.com>
2023-11-30 22:58:54 -0600

    Complete 1st order function

--- src/order.js
+++ src/order.js
@@ -1,6 +1,25 @@
+function takeOrder(order, orderTypeList) {
+  if (orderTypeList.length < 3) {
+  orderTypeList = orderTypeList.push(order);
+}}
+
+
+function refundOrder() {
+
+}
+
+
+function listItems() {
+
+}
+
+
+function searchOrder() {
+
+}
 
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
