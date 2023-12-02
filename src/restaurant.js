43d13bf922e492725b880811b08c2a2733ef803f
Tayla Phillips <richardstayla@gmail.com>
2023-12-02 11:31:00 -0600

    Complete 1st restaurant function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,19 @@
-function createRestaurant() {
-
+function createRestaurant(restaurantName) {
+//create an object to be stored in the variable defined in the test.
+//the object should include the keys name and menus (menus key
+//should hold an object with the keys breakfast, lunch, and dinner)
+  return {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }; 
 };
+
+// console.log('Pizza restaurant name: ', restaurantName.name);
+// console.log(createRestaurant('Sexy Pizza'));
 
 function addMenuItem() {
 
@@ -17,7 +30,7 @@ function checkForFood() {
 
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant, 
   // addMenuItem,
   // removeMenuItem,
   // checkForFood
