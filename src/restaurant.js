e392b20e4e68386612d1c7940a608b761a38efe6
Tayla Phillips <richardstayla@gmail.com>
2023-12-02 16:49:49 -0600

    Complete 4th restaurant function

--- src/restaurant.js
+++ src/restaurant.js
@@ -11,11 +11,6 @@ function createRestaurant(restaurantName) {
 
 
 function addMenuItem(restaurant, menuItem) {
-  // console.log(restaurant.menus);
-  // console.log(menuItem.type);
-  // console.log(restaurant.menus.lunch);
-  // console.log(restaurant.menus.lunch.includes(menuItem));
-
   if (menuItem.type === 'lunch' && restaurant.menus.lunch.includes(menuItem) === false) { //is not present in the menus.lunch array// 
     restaurant.menus.lunch.push(menuItem);
   }
@@ -27,19 +22,48 @@ function addMenuItem(restaurant, menuItem) {
   };
 };
 
-function removeMenuItem() {
+function removeMenuItem(restaurant, menuItem, menuItemType) {
+  // console.log('pizzaRestaurant: ', restaurant);
+  // console.log('pizzaRestaurant.menus object: ', restaurant.menus);
+  // console.log('menuItemType: ', menuItemType);
+  // console.log('pizzaRestaurant.menus.breakfast: ', restaurant.menus.breakfast)
+  
+  // var onBreakfastMenu = restaurant.menus.breakfast.find(function isOnThisMenu(food){
+  //   return  food.type === type;
+  // })
+
+  // var onMenu = restaurant.find(function isOnMenu(menuItem){
+  //   return menuItem.name === menuItem;
+  // })
+
+  // if (onBreakfastMenu === true) { //&& onMenu === true) {
+  //   console.log('ON BREAKFAST MENU')
 
 };
 
-function checkForFood() {
 
+
+function checkForFood(restaurant, menuItem) {
+  // console.log(restaurant);
+// console.log(restaurant.menus);
+// console.log(restaurant.menus.breakfast);
+// console.log(restaurant.menus.breakfast[0]);
+// console.log(restaurant.menus.breakfast[0].name);
+  if (restaurant.menus.breakfast.includes(menuItem) ||
+      restaurant.menus.lunch.includes(menuItem) ||
+      restaurant.menus.dinner.includes(menuItem)) {
+        return `Yes, we're serving ${menuItem.name} today!`
+      } else {
+        return `Sorry, we aren't serving ${menuItem.name} today.`
+      }
 };
+
 
 
 
 module.exports = {
   createRestaurant, 
   addMenuItem,
-  // removeMenuItem,
-  // checkForFood
+  removeMenuItem,
+  checkForFood
 }
