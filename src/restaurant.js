f2eb8f5a566136c452abc343f9e74295d5acfea5
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-24 21:37:54 -0600

    RefactorremoveMenuItem and checkforFood to reduce redundancies

--- src/restaurant.js
+++ src/restaurant.js
@@ -11,33 +11,32 @@ function createRestaurant(name) {
 
 
 function addMenuItem(restaurant, menuItem) {
-  // console.log(restaurant.menus[menuItem.type])
   if (!restaurant.menus[menuItem.type].includes(menuItem)) {
     restaurant.menus[menuItem.type].push(menuItem)
   }
 };
 
 function removeMenuItem(restaurant, menuItem, menuItemType) {
-  if (restaurant.menus[menuItemType].length === 0) {
-    return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`  
+  let message = ''
+  if (!restaurant.menus[menuItemType].length) {
+    message = `Sorry, we don't sell ${menuItem}, try adding a new recipe!`  
   };
-  
-  for (var i = 0; i <= restaurant.menus[menuItemType].length; i ++) {
-    if (menuItem === restaurant.menus[menuItemType][i].name) {
-      restaurant.menus[menuItemType].splice(i, 1);
-      return `No one is eating our ${menuItem} - it has been removed from the ${menuItemType} menu!`
-    } else {
-        return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`
+
+  restaurant.menus[menuItemType].forEach((object) => {
+    if (menuItem === object.name) {
+      restaurant.menus[menuItemType].splice(restaurant.menus[menuItemType].indexOf(object), 1);
+      message = `No one is eating our ${menuItem} - it has been removed from the ${menuItemType} menu!`
+    }else {
+      message = `Sorry, we don't sell ${menuItem}, try adding a new recipe!`
     }
-  };
+  })
+  return message
 };
 
 
 
 function checkForFood(restaurant, menuItem) {
-  if (restaurant.menus.breakfast.includes(menuItem) ||
-      restaurant.menus.lunch.includes(menuItem) ||
-      restaurant.menus.dinner.includes(menuItem)) {
+  if (restaurant.menus[menuItem.type].includes(menuItem)) {
         return `Yes, we're serving ${menuItem.name} today!`
       } else {
         return `Sorry, we aren't serving ${menuItem.name} today.`
